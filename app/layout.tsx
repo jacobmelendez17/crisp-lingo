b98e648f5d0937a0727be6710e4c6e34fd88fde2
jacobmelendez17 <nerdalert46@gmail.com>
2025-09-21 14:22:30 -0700

    Configured drizzle and neon database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Patrick_Hand } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
 const font = Patrick_Hand({ subsets: ['latin'], weight: ['400'] });
@@ -15,8 +16,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={font.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={font.className}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
