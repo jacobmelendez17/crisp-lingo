0deab106b7d1f03624ad314da495b4058946abbb
jacobmelendez17 <nerdalert46@gmail.com>
2025-09-20 23:59:01 -0700

    Started landing page skeleton

---
+++ app/(landing)/footer.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export const Footer = () => {
+	return (
+		<footer className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
+			<div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
+				<p>Something</p>
+			</div>
+		</footer>
+	);
+};
