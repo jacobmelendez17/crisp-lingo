6e91b5e5e76034b5b841e5cb2f2b5f6cf434399f
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-06 23:01:14 -0700

    Changed progress levels to five grid. Changed level progress background

--- app/(app)/(main)/components/level-progress-card.tsx
+++ app/(app)/(main)/components/level-progress-card.tsx
@@ -2,7 +2,7 @@ import { CardShell } from './card-shell';
 
 export function LevelProgressCard() {
 	return (
-		<CardShell title="Level Progress" className="min-h-[720px] lg:min-h-[820px]">
+		<CardShell title="Level Progress" className="min-h-[720px] bg-white lg:min-h-[820px]">
 			<div>Level Progress Card</div>
 		</CardShell>
 	);
--- app/(app)/(main)/components/progress-card.tsx
+++ app/(app)/(main)/components/progress-card.tsx
@@ -3,7 +3,7 @@ import { CardShell } from './card-shell';
 export function ProgressCard() {
 	return (
 		<CardShell title="Progress">
-			<div className="grid gap-6 sm:grid-cols-2 xl:grid-cols-3">
+			<div className="grid gap-6 sm:grid-cols-2 xl:grid-cols-5">
 				<CardShell title="Beginner Pup" className="bg-white">
 					<div> </div>
 				</CardShell>
