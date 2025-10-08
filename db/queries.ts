05bf21b640eef11f0930ce9f6aac2ef7133cf0a9
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-07 23:25:11 -0700

    Added first query function

--- db/queries.ts
+++ db/queries.ts
@@ -3,3 +3,23 @@ import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 
 import db from "./drizzle";
+import {
+    userProgress
+} from "@/db/schema";
+
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeLevel: true,
+        }
+    });
+
+    return data;
+})
