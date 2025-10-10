589d8237c17d4048a1a4817bf51ba1197ba2f6a3
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-09 22:54:13 -0700

    Added learn session header

--- app/learn/header.tsx
+++ app/learn/header.tsx
@@ -1,3 +1,18 @@
 import { X } from 'lucide-react';
 import { Progress } from '@/components/ui/progress';
 import Image from 'next/image';
+
+type Props = {
+	percentage: number;
+};
+
+export const Header = ({ percentage }: Props) => {
+	const { open } = useExitModal();
+
+	return (
+		<header className="justify between mx-auto flex w-full max-w-[1140px] items-center gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+			<X onClick={open} className="cursor-pointer text-slate-500 transition hover:opacity-75" />
+			<Progress value={percentage} />
+		</header>
+	);
+};
