10fdacfa16198557b6abdb4b5f4e6f0f9b50e6a2
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-09 23:27:23 -0700

    Updated lesson page

--- app/learn/lesson/page.tsx
+++ app/learn/lesson/page.tsx
@@ -1,8 +1,19 @@
 import { getBatch, getUserProgress } from '@/db/queries';
 import { LearnSession } from './learn-session';
 
-export default async function LessonPage() {
+const LessonPage = async () => {
+	const userProgressData = getUserProgress();
+	const batchData = getBatch();
+
+	const [userProgress, batch] = await Promise.all([userProgressData, batchData]);
+
+	if (!batch || !userProgress) {
+		return null;
+	}
+
 	const initialPercentage = 100;
 
 	return <LearnSession initialPercentage={initialPercentage} />;
-}
+};
+
+export default LessonPage;
