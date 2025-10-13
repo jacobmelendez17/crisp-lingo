9d8648562e11be44dd44ed05f49e717f978b5c36
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-12 23:58:07 -0700

    Made template for context page

--- app/learn/lesson/context.tsx
+++ app/learn/lesson/context.tsx
@@ -1,7 +1,26 @@
 'use client';
 
 import type { VocabItem } from '../types';
+import { CardShell } from '@/components/card-shell';
 
 export default function ContextTab({ item }: { item: VocabItem }) {
-	return <div>Context Tab!</div>;
+	return (
+		<section className="mt-8 rounded-[28px] border border-black/5 p-6">
+			<div className="grid gap-8 sm:grid-cols-[1fr_2fr]">
+				<CardShell title="Use Patterns" className="bg-transparent">
+					<h2 className="dont-semibold mb-2 text-2xl">Blah blah blah</h2>
+				</CardShell>
+				<CardShell title=" " className="bg-transparent">
+					<p className="leading-relaxed text-neutral-800">
+						{item.meaning || 'No meaning made for this word yet'}
+					</p>
+				</CardShell>
+			</div>
+			<CardShell title="Example Sentences" className="mt-5 bg-transparent">
+				<p className="leading-relaxed text-neutral-800">
+					{item.meaning || 'No meaning made for this word yet'}
+				</p>
+			</CardShell>
+		</section>
+	);
 }
