7c0db80998c7eb065bad87dfdea3faf6c9941ba8
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-15 22:52:19 -0700

    Added pages for all header links

---
+++ app/(app)/grammar/page.tsx
@@ -0,0 +1,7 @@
+export default async function GrammarPage() {
+	return (
+		<main className="mx-auto w-full max-w-[1400px] px-4 py-6 lg:px-6">
+			<div className="grid gap-6 pt-10 lg:grid-cols-[1fr_360px]">Grammar Page</div>
+		</main>
+	);
+}
