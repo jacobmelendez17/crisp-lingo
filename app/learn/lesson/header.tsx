1eef7b0c7e3b837087adf0524ca359e70d0f2616
jacobmelendez17 <nerdalert46@gmail.com>
2025-10-25 19:45:31 -0700

    Added inbox and settings icons

--- app/learn/lesson/header.tsx
+++ app/learn/lesson/header.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link';
-import { X, ArrowLeft, Mail } from 'lucide-react';
-import { Progress } from '@/components/ui/progress';
+import { ArrowLeft, Mail } from 'lucide-react';
 import { useExitModal } from '@/store/use-exit-modal';
 
 export const Header = () => {
 	const { open } = useExitModal();
 
 	return (
-		<header className="justify between mx-auto flex w-full max-w-[1130px] items-center gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+		<header className="mx-auto flex w-full max-w-[1130px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
 			<Link href="/dashboard">
-				<ArrowLeft className="w06 h-6 cursor-pointer text-slate-600 transition hover:opacity-75" />
+				<ArrowLeft className="h-6 w-6 cursor-pointer text-slate-600 transition hover:opacity-75" />
 			</Link>
 		</header>
 	);
--- app/learn/review/header.tsx
+++ app/learn/review/header.tsx
@@ -1,6 +1,8 @@
-import { X } from 'lucide-react';
+import { X, Settings } from 'lucide-react';
 import { Progress } from '@/components/ui/progress';
 import { useExitModal } from '@/store/use-exit-modal';
+import Link from 'next/link';
+import Image from 'next/image';
 
 type Props = {
 	percentage: number;
@@ -13,9 +15,16 @@ export const Header = ({ percentage }: Props) => {
 	const { open } = useExitModal();
 
 	return (
-		<header className="justify between max-w-100% mx-auto flex w-full items-center gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+		<header className="max-w-100% mx-auto flex w-full items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
 			<X onClick={open} className="cursor-pointer text-slate-500 transition hover:opacity-75" />
 			<Progress value={percentage} />
+			<Link href="/dashboard">
+				<Image className="hover:opacity-75" src="/inbox.svg" alt="Inbox" height={70} width={70} />
+			</Link>
+			<Settings
+				onClick={open}
+				className="cursor-pointer text-slate-500 transition hover:opacity-75"
+			/>
 		</header>
 	);
 };
